8ddf9d8bd0b2f12e96e64b5e9cd173b984678458
dilliram-code <chaudharydr23@gmail.com>
2025-11-13 08:24:59 +0545

    load the pkl file into the working file

--- application.py
+++ application.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
+
+# import ridge regressor and standard scaler pickle
+ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl', 'rb')) 
 
 @app.route("/")
 def hello_world():
